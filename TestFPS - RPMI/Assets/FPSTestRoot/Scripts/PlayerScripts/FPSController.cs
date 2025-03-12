5d795d995836eeb19c238f6576de239651354c2e
Jaume127 <jgelabert.petit@gmail.com>
2025-03-12 11:56:54 +0100

    12/03

---
+++ TestFPS - RPMI/Assets/FPSTestRoot/Scripts/PlayerScripts/FPSController.cs
@@ -0,0 +1,104 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem; //Libreria para trabajar con el NewInputSystem
+
+public class FPSController : MonoBehaviour
+{
+
+    [Header("Movement & Look Stats")]
+    [SerializeField] GameObject camHolder; //Ref al objeto que almacena la cámara (Ojos del player)
+    public float speed; //Velocidad normal
+    public float sprintSpeed; //Velocidad en sprint
+    public float crouchSpeed; //Velocidad agachado
+    public float maxForce = 1; //Limite de aceleración máxima
+    public float sensitivity = 0.1f; //Sensibilidad aplicada al input de observar
+
+    [Header("Jumping Stats")]
+    public float jumpForce;
+
+    //Referencias privadas (GetComponent)
+    private Rigidbody playerRb;
+    private Animator anim;
+    //Referencias privadas del Input
+    Vector2 moveInput;
+    Vector2 lookInput;
+    float lookRotation; //Valor de rotación que puede ser utilizado para la dirección de movimiento
+
+
+    private void Awake()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+        camHolder = GameObject.Find("CameraHolder");
+    }
+
+    void Start()
+    {
+        //Lock del cursor al centro de la cámara y ocultarlo
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        Movement();
+    }
+
+
+    private void LateUpdate()
+    {
+        CameraLook();
+    }
+
+
+    void Movement()
+    {
+        Vector3 currentVelocity = playerRb.velocity; //Velocidad actual del player
+        Vector3 targetVelocity = new Vector3(moveInput.x, 0, moveInput.y); //Velocidad hacia la que queremos que se mueva el player
+        targetVelocity *= speed;
+
+        //Alinear la dirección con la orientación correcta (de local a global)
+        targetVelocity = transform.TransformDirection(targetVelocity);
+
+        //Calcular las fuerczas que afectan al movimiento
+        Vector3 velocityChange = (targetVelocity - currentVelocity);
+        velocityChange = new Vector3(velocityChange.x, 0, velocityChange.z); //Hace que la aceleración no afecte en vertical
+        Vector3.ClampMagnitude(velocityChange, maxForce);
+
+        //Aplicamos el movimiento
+        playerRb.AddForce(velocityChange , ForceMode.VelocityChange);
+
+    }
+
+    void CameraLook()
+    {
+        //Girar (Gira el personaje en horizontal)
+        transform.Rotate(Vector3.up * lookInput.x * sensitivity);
+        //Mirar (Gira la cámara en vertical)
+        lookRotation += (-lookInput.y * sensitivity);
+        lookRotation = Mathf.Clamp(lookRotation, -90, 90); //Restringe el valor de lookRotation entre dos valores mínimo/máximo
+        camHolder.transform.eulerAngles = new Vector3(lookRotation, camHolder.transform.eulerAngles.y, camHolder.transform.eulerAngles.z);
+    }
+
+    #region Input Methods
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }
+
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        lookInput = context.ReadValue<Vector2>();
+    }
+    #endregion
+
+
+}
